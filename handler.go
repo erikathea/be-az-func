25234fa7429229b68afc629236e8b51c1517cfe7
erikathea <ethajes@gmail.com>
2024-07-18 13:41:57 -0300

    removed offline data pre-processing code

--- handler.go
+++ handler.go
@@ -1,19 +1,15 @@
 package main
 
 import (
-	"crypto/rand"
 	"database/sql"
-	"encoding/base64"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
-	"math"
 	"net/http"
 	"os"
 
 	"github.com/erikathea/migp-go/pkg/migp"
-	"github.com/erikathea/migp-go/pkg/mutator"
 	_ "github.com/lib/pq"
 )
 
@@ -26,7 +22,6 @@ type kvStore struct {
 func newKVStore(db *sql.DB) (*kvStore, error) {
 	kv := &kvStore{db: db}
 
-	// Create the table if it doesn't exist
 	query := `
 	CREATE TABLE IF NOT EXISTS kv_store (
 		id TEXT NOT NULL,
@@ -52,40 +47,6 @@ func newKVStore(db *sql.DB) (*kvStore, error) {
 	return kv, nil
 }
 
-// Put a value at key id and replace any existing value.
-func (kv *kvStore) Put(id string, value []byte) error {
-	query := `
-	INSERT INTO kv_store (id, value) VALUES ($1, $2)
-	ON CONFLICT (id) DO UPDATE SET value = $2;`
-	_, err := kv.db.Exec(query, id, value)
-	return err
-}
-
-// Put a value at key id and replace any existing value.
-func (kv *kvStore) insertShadow(id string, value []byte) error {
-	query := `
-	INSERT INTO kv_store_shadow (id, value) VALUES ($1, $2)
-	ON CONFLICT (id, value) DO NOTHING;`
-	_, err := kv.db.Exec(query, id, value)
-	return err
-}
-
-// Append a value to any existing value at key id.
-func (kv *kvStore) Append(id string, value []byte) error {
-	query := `SELECT value FROM kv_store WHERE id = $1`
-	var existingValue []byte
-	err := kv.db.QueryRow(query, id).Scan(&existingValue)
-	if err != nil {
-		if err == sql.ErrNoRows {
-			return kv.Put(id, value)
-		}
-		return err
-	}
-
-	newValue := append(existingValue, value...)
-	return kv.Put(id, newValue)
-}
-
 // Get returns the value in the key identified by id.
 func (kv *kvStore) Get(id string) ([]byte, error) {
 	query := `SELECT value FROM kv_store WHERE id = $1`
@@ -100,14 +61,6 @@ func (kv *kvStore) Get(id string) ([]byte, error) {
 	return value, nil
 }
 
-// checkIfUnique checks if the value for a given id is unique in the shadow table.
-func (kv *kvStore) checkIfUnique(id string, value []byte) bool {
-	query := `SELECT 1 FROM kv_store_shadow WHERE id = $1 AND value = $2`
-	var exists int
-	err := kv.db.QueryRow(query, id, value).Scan(&exists)
-	return err == sql.ErrNoRows
-}
-
 // newServer returns a new server initialized using the provided configuration
 func newServer(cfg migp.ServerConfig) (*server, error) {
 	migpServer, err := migp.NewServer(cfg)
@@ -118,7 +71,7 @@ func newServer(cfg migp.ServerConfig) (*server, error) {
 	dbConnectionString := os.Getenv("DB_CONNECTION_ST")
 	if dbConnectionString == "" {
 		log.Println("DB_CONNECTION_ST environment variable not set. Using default localhost connection string.")
-		dbConnectionString = "user=cs-db password=hacker dbname=cs-db sslmode=disable host=localhost"
+		dbConnectionString = "user=user password=pw dbname=db sslmode=disable host=localhost"
 	}
 
 	log.Printf("Using database connection string: %s", dbConnectionString)
@@ -153,85 +106,6 @@ func (s *server) handler() http.Handler {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/api/query", s.handleEvaluate)
 	return mux
-}
-
-// GenerateRandomString generates a random λ-bits long string
-func GenerateRandomString(bits int) ([]byte, error) {
-	bytes := int(math.Ceil(float64(bits) / 8.0))
-	randomBytes := make([]byte, bytes)
-
-	_, err := rand.Read(randomBytes)
-	if err != nil {
-		return nil, err
-	}
-
-	return randomBytes, nil
-}
-
-// insert encrypts a credential pair and stores it in the configured KV store
-func (s *server) insert(username, password, metadata []byte, numVariants int, includeUsernameVariant, phaseOne bool) error {
-	var (
-		newEntry []byte
-		err      error
-	)
-	bucketIDHex := migp.BucketIDToHex(s.migpServer.BucketID(username))
-	log.Println("----ID ", bucketIDHex)
-	if phaseOne {
-		newEntry, err := s.migpServer.EncryptBucketEntry(username, password, migp.MetadataBreachedPassword, metadata)
-		if err != nil {
-			return err
-		}
-
-		err = s.kv.Append(bucketIDHex, newEntry)
-		if err != nil {
-			return err
-		}
-		s.kv.insertShadow(bucketIDHex, newEntry)
-		log.Println("newEntry ", base64.StdEncoding.EncodeToString(newEntry))
-
-		if includeUsernameVariant {
-			newEntry, err = s.migpServer.EncryptBucketEntry(username, nil, migp.MetadataBreachedUsername, metadata)
-			if err != nil {
-				return err
-			}
-
-			err = s.kv.Append(bucketIDHex, newEntry)
-			if err != nil {
-				return err
-			}
-			s.kv.insertShadow(bucketIDHex, newEntry)
-			log.Println("-- includeUsernameVariant ", base64.StdEncoding.EncodeToString(newEntry))
-		}
-		return nil
-	} else {
-		passwordVariants := mutator.NewRDasMutator().Mutate(password, numVariants)
-		for _, variant := range passwordVariants {
-			newEntry, err = s.migpServer.EncryptBucketEntry(username, variant, migp.MetadataSimilarPassword, metadata)
-			// Ensure the value is unique before appending
-			for !s.kv.checkIfUnique(bucketIDHex, newEntry) {
-				randomString, _ := GenerateRandomString(256)
-				altVariant := mutator.NewRDasMutator().Mutate(randomString, 1)
-				log.Println(".    altVariant - ", string(altVariant[0]))
-				newEntry, err = s.migpServer.EncryptBucketEntry(username, altVariant[0], migp.MetadataSimilarPassword, metadata)
-			}
-			if err != nil {
-				return err
-			}
-			err = s.kv.Append(bucketIDHex, newEntry)
-			if err != nil {
-				return err
-			}
-			s.kv.insertShadow(bucketIDHex, newEntry)
-			log.Println("-- password variant ", base64.StdEncoding.EncodeToString(newEntry))
-
-		}
-	}
-
-	bucketContents, err := s.kv.Get(bucketIDHex)
-	log.Println("content ", base64.StdEncoding.EncodeToString(bucketContents))
-	log.Println("ID ", bucketIDHex)
-
-	return nil
 }
 
 // handleIndex returns a welcome message
